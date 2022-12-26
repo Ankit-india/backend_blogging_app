05dac2f74ea55922f67b238bcdd65fb2a5be0cae
Ankit Kumar <ankitkr00.india@gmail.com>
2022-12-26 23:06:53 +0530

    day 2 - User API completed and tested sucessfully

--- src/main/java/com/devankit/blog/controllers/UserController.java
+++ src/main/java/com/devankit/blog/controllers/UserController.java
@@ -1,13 +1,20 @@
 package com.devankit.blog.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.devankit.blog.payloads.ApiResponse;
 import com.devankit.blog.payloads.UserDto;
 import com.devankit.blog.services.UserService;
 
@@ -27,9 +34,32 @@ public class UserController {
 		return new ResponseEntity<>(createdUserDto, HttpStatus.CREATED);
 	}
 	
-	// GET - get user
+	// GET - get all user
+	@GetMapping("/")
+	public ResponseEntity<List<UserDto>> getAllUser()
+	{
+		return ResponseEntity.ok(this.userService.getAllUser());
+	}
+	// GET - get single user
+	@GetMapping("/{userId}")
+	public ResponseEntity<UserDto> getSingleUser(@PathVariable Integer userId)
+	{
+		return ResponseEntity.ok(this.userService.getUserById(userId));
+	}
 	
 	// PUT - update user
+	@PutMapping("/{userId}")
+	public ResponseEntity<UserDto> updateUser(@RequestBody UserDto userDto, @PathVariable Integer userId)
+	{
+		UserDto updatedUser = this.userService.updateUser(userDto, userId);
+		return ResponseEntity.ok(updatedUser);
+	}
 	
 	// DELETE - delete user
+	@DeleteMapping("/{userId}")
+	public ResponseEntity<ApiResponse> deleteUser(@PathVariable("userId") Integer uid)
+	{
+		this.userService.deleteUser(uid);
+		return new ResponseEntity<ApiResponse>(new ApiResponse("User deleted sucessfully", true), HttpStatus.OK);
+	}
 }
