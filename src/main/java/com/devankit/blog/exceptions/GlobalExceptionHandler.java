0728fb99180b90138c109c2d31af59c1e7786c38
Ankit Kumar <ankitkr00.india@gmail.com>
2022-12-29 02:21:51 +0530

    day 3... User API completed!

--- src/main/java/com/devankit/blog/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/devankit/blog/exceptions/GlobalExceptionHandler.java
@@ -1,7 +1,12 @@
 package com.devankit.blog.exceptions;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -16,5 +21,17 @@ public class GlobalExceptionHandler {
 		String message = ex.getMessage();
 		ApiResponse apiRespone = new ApiResponse(message, false);
 		return new ResponseEntity<ApiResponse>(apiRespone, HttpStatus.NOT_FOUND);
+	}
+	
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<Map<String, String>> handleMethodArgumentNotValidException(MethodArgumentNotValidException e)
+	{
+		Map<String, String> response = new LinkedHashMap<>();
+		e.getBindingResult().getAllErrors().forEach((err)->{
+			String responseFieldKeyName = ((FieldError)err).getField();
+			String responseMessage = err.getDefaultMessage();
+			response.put(responseFieldKeyName, responseMessage);
+		});
+		return new ResponseEntity<Map<String, String>>(response, HttpStatus.BAD_REQUEST);
 	}
 }
--- src/main/java/com/devankit/blog/payloads/UserDto.java
+++ src/main/java/com/devankit/blog/payloads/UserDto.java
@@ -1,5 +1,10 @@
 package com.devankit.blog.payloads;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -11,8 +16,15 @@ import lombok.Setter;
 public class UserDto {
 
 	private int id;
+	
+	@NotEmpty
+	@Size(min = 3, message = "User name must be greater than 3")
 	private String name;
+	@Email(message = "Entered email is not valid")
 	private String email;
+	@NotEmpty
+	@Size(min = 3, max = 10, message = "Password must be greater than 3 and less than 10")
 	private String password;
+	@NotEmpty
 	private String about;
 }
